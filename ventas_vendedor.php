325aab34b52bf2938b0fac2730639c6b0c4249e2
Ycosio <ycosio@ucol.mx>
2016-11-25 04:18:42 -0600

    Proyecto terminado corregido index nombre cliente

--- ventas_vendedor.php
+++ ventas_vendedor.php
@@ -6,13 +6,13 @@ $id=$_GET['id'];
 $contador=0;
 
 $bd = new SQLite3('PI.db'); 
-$results = $bd->query('SELECT Productos.*, Ventas.*, Ventas.id AS id_Venta FROM Productos
+$results = $bd->query('SELECT Productos.*, Ventas.*, Ventas.id AS id_Venta, Cliente.nombre AS Cliente FROM Productos
 	LEFT JOIN Ventas ON Productos.id=Ventas.id_Producto
 	LEFT JOIN Cliente ON Ventas.id_cliente=Cliente.id
 	WHERE Cliente.id_vendedor='.$id.' and Ventas.pagosPendientes>0');
 $cadena='{"records":[';
 while ($row = $results->fetchArray()) {
-	$cadena=$cadena.'{"id":"'.$row['id_Producto'].'","nombre":"'.$row['nombre'].'","precio":"'.$row['precio'].'","cantidad":"'.$row['cantidad'].'","descripcion":"'.$row['descripcion'].'","pagosPendientes":"'.$row["pagosPendientes"].'","montoPendiente":"'.$row['montoPendiente'].'","montoPorPago":"'.$row["montoPorPago"].'","siguientePago":"'.$row["siguientePago"].'","id_Venta":"'.$row["id_Venta"].'","plazo":"'.$row["plazo"].'"},';
+	$cadena=$cadena.'{"id":"'.$row['id_Producto'].'","nombre":"'.$row['nombre'].'","precio":"'.$row['precio'].'","cantidad":"'.$row['cantidad'].'","descripcion":"'.$row['descripcion'].'","pagosPendientes":"'.$row["pagosPendientes"].'","montoPendiente":"'.$row['montoPendiente'].'","montoPorPago":"'.$row["montoPorPago"].'","siguientePago":"'.$row["siguientePago"].'","id_Venta":"'.$row["id_Venta"].'","plazo":"'.$row["plazo"].'","Cliente":"'.$row["Cliente"].'"},';
 	$contador++;
 }
 $cadena=substr($cadena,0,-1);
